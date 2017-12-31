e16cac85fc4ea7aa4a28cd29e7cdf899316b2526
Juan Sebastian Bedoya Arevalo <juansb827@gmail.com>
2017-12-31 16:50:53 -0500

    allow token header

--- server/config/initializers/server.js
+++ server/config/initializers/server.js
@@ -18,7 +18,7 @@ function start(callback) {
 
     app.use(function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization,x-access-token");
         if (req.method === "OPTIONS") {
             res.header("Access-Control-Allow-Methods", "PUT,POST,DELETE");
             return res.status(200).json({});
